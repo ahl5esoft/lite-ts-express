06731d422b06423ac9cb3d9fd1db8a77edd0f19a
lin-zhi-ming <1433757137@qq.com>
2023-03-22 16:20:34 +0800

    feat:添加apiFactory

---
+++ src/api-factory.ts
@@ -0,0 +1,58 @@
+
+import { IDirectory } from 'lite-ts-fs';
+import Container from 'typedi';
+
+import { ApiFactoryBase } from './api-factory-base';
+import { ErrorCode } from './error-code';
+import { IApi } from './i-api';
+import { CustomError } from './post-option';
+
+const invalidAPIError = new CustomError(ErrorCode.api);
+const invalidAPI: IApi = {
+    call() {
+        throw invalidAPIError;
+    },
+};
+
+export class ApiFactory extends ApiFactoryBase {
+    public constructor(
+        private m_APICtors: { [key: string]: { [key: string]: Function; }; }
+    ) {
+        super();
+    }
+
+    public build(endpoint: string, apiName: string) {
+        const apiCtors = this.m_APICtors[endpoint];
+        if (!apiCtors)
+            return invalidAPI;
+
+        const apiCtor = apiCtors[apiName];
+        if (!apiCtor)
+            return invalidAPI;
+
+        const api = Container.get<IApi>(apiCtor);
+        Container.remove(apiCtor);
+        return api;
+    }
+}
+
+export async function createApiFactory(dir: IDirectory) {
+    let apiCtors = {};
+    const dirs = await dir.findDirectories();
+    for (const r of dirs) {
+        const files = await r.findFiles();
+        apiCtors[r.name] = files.reduce((memo: { [key: string]: Function; }, cr) => {
+            if (cr.name.includes('_it') || cr.name.includes('_test') || cr.name.includes('.d.ts'))
+                return memo;
+
+            const api = require(cr.path);
+            if (!api.default)
+                throw new Error(`未导出default: ${cr.path}`);
+
+            const name = cr.name.split('.')[0];
+            memo[name] = api.default;
+            return memo;
+        }, {});
+    }
+    return new ApiFactory(apiCtors);
+}
