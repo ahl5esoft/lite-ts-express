79c59007082ee12bc2c7f2528e2e2c841472c9b7
linyimin <ahl5esoft@gmail.com>
2023-04-07 11:56:51 +0800

    docs: v1.5.1

--- src/body-parser-option.ts
+++ src/body-parser-option.ts
@@ -2,18 +2,22 @@ import { json, OptionsJson, OptionsUrlencoded, urlencoded } from 'body-parser';
 
 import { ExpressOption } from './option';
 
-export function bodyParserFormExpressOption(option: OptionsUrlencoded): ExpressOption {
+export function bodyParserFormExpressOption(option?: OptionsUrlencoded): ExpressOption {
     return app => {
         app.use(
-            urlencoded(option)
+            urlencoded(option ?? {
+                extended: true
+            })
         );
     };
 }
 
-export function bodyParserJsonExpressOption(option: OptionsJson): ExpressOption {
+export function bodyParserJsonExpressOption(option?: OptionsJson): ExpressOption {
     return app => {
         app.use(
-            json(option)
+            json(option ?? {
+                limit: '16mb'
+            })
         );
     };
 }
